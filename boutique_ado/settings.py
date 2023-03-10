37da125090fcbd9504bfc8c02ac75e6196d4a270
Paula da Silva <carolpaulag@gmail.com>
2023-03-10 10:09:00 +0000

    updating settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_bootstrap4',
-    'crispy_forms'
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
